fd171713fffb16a59125f89d038ce1bb1300792b
theg <52027622+theg1239@users.noreply.github.com>
2024-09-20 17:18:46 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
+// Set up the PostgreSQL connection pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'codex', 
-  port: 5432,
+  connectionString: process.env.DATABASE_URL || 'postgres://u55nenbuc8tput:p8527791571ff07ac1151b53bc99144bb5abd63902d8a628007ebbe0730eb2992@c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/dbname',
+  ssl: {
+    rejectUnauthorized: false, // Important for Heroku/RDS managed databases with SSL
+  },
 });
 
 export default pool;
