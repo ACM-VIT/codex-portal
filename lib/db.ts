7c4218a1298dafbcfef43584b74fb2e95cae12b5
theg <52027622+theg1239@users.noreply.github.com>
2024-09-20 18:13:56 +0530

    fixed db name

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 // Set up the PostgreSQL connection pool
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://u55nenbuc8tput:p8527791571ff07ac1151b53bc99144bb5abd63902d8a628007ebbe0730eb2992@c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/dbname',
+  connectionString: process.env.DATABASE_URL || 'postgres://u55nenbuc8tput:p8527791571ff07ac1151b53bc99144bb5abd63902d8a628007ebbe0730eb2992@c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/deveidao82dcvl',
   ssl: {
     rejectUnauthorized: false, // Important for Heroku/RDS managed databases with SSL
   },
