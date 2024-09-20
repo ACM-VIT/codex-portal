eea4fedf9ba8640ca4ef209ebcd736e15a3b1b6b
theg <52027622+theg1239@users.noreply.github.com>
2024-09-20 18:32:18 +0530

    fixx

--- next.config.mjs
+++ next.config.mjs
@@ -1,46 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/api/sse-leaderboard",
-          destination: '/', // redirect to home if accessed during build
-          permanent: false,
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "no-store", 
-            },
-          ],
-        },
-        {
-          source: "/api/leaderboard",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "no-store",
-            },
-          ],
-        },
-        {
-          source: "/api/:path*",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "no-store", 
-            },
-          ],
-        },
-      ];
-    },
-    
-    // Other Next.js configurations can go here
-    reactStrictMode: true,
-  
-    // Enable server-side events (SSE) if necessary
-    experimental: {
-      serverComponentsExternalPackages: ["pg"], // Ensuring external packages like pg are allowed
-    },
+    // Any other configuration
   };
   
   export default nextConfig;
