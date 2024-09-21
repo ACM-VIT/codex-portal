947a89204ed3400ee16f766714369c6f13ecc687
theg <52027622+theg1239@users.noreply.github.com>
2024-09-22 04:22:12 +0530

    up

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,10 @@
-// lib/db.ts
-
 import { Pool } from 'pg';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var _pool: Pool | undefined;
-}
-
-let pool: Pool;
-
-if (!global._pool) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    max: 200, // Adjust this number as needed
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  });
-  if (process.env.NODE_ENV !== 'production') {
-    global._pool = pool;
-  }
-} else {
-  pool = global._pool;
-}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
